58201628f8b26b31ca8cae7f99a7a053cc945c5a
Benjamin Stevens <B3nbinoh@gmail.com>
2025-03-27 14:35:25 -0600

    - Completed jquery refactoring assignment

--- practice_projects/inventory_order_form_start_code/javascripts/inventory.js
+++ practice_projects/inventory_order_form_start_code/javascripts/inventory.js
@@ -1,38 +1,39 @@
-var inventory;
+let inventory;
 
-(function() {
+(function () {
   inventory = {
     lastId: 0,
     collection: [],
-    setDate: function() {
-      var date = new Date();
-      $("#order_date").text(date.toUTCString());
+    setDate: function () {
+      let date = new Date();
+      document.getElementById("order_date").textContent = date.toUTCString();
     },
-    cacheTemplate: function() {
-      var $iTmpl = $("#inventory_item").remove();
-      this.template = $iTmpl.html();
+    cacheTemplate: function () {
+      let iTmpl = document.getElementById("inventory_item");
+      iTmpl.remove();
+      this.template = Handlebars.compile(iTmpl.getHTML());
     },
-    add: function() {
+    add: function () {
       this.lastId++;
-      var item = {
+      let item = {
         id: this.lastId,
         name: "",
         stock_number: "",
-        quantity: 1
+        quantity: 1,
       };
       this.collection.push(item);
 
       return item;
     },
-    remove: function(idx) {
-      this.collection = this.collection.filter(function(item) {
+    remove: function (idx) {
+      this.collection = this.collection.filter(function (item) {
         return item.id !== idx;
       });
     },
-    get: function(id) {
-      var found_item;
+    get: function (id) {
+      let found_item;
 
-      this.collection.forEach(function(item) {
+      this.collection.forEach(function (item) {
         if (item.id === id) {
           found_item = item;
           return false;
@@ -41,49 +42,76 @@ var inventory;
 
       return found_item;
     },
-    update: function($item) {
-      var id = this.findID($item),
-          item = this.get(id);
+    update: function (item) {
+      //Get id of item and
+      let id = this.findID(item);
+      let itemObject = this.get(id);
 
-      item.name = $item.find("[name^=item_name]").val();
-      item.stock_number = $item.find("[name^=item_stock_number]").val();
-      item.quantity = $item.find("[name^=item_quantity]").val();
+      itemObject.name = item.querySelector("[name^=item_name]").value;
+      itemObject.stock_number = item.querySelector(
+        "[name^=item_stock_number]"
+      ).value;
+      itemObject.quantity = item.querySelector("[name^=item_quantity]").value;
     },
-    newItem: function(e) {
+    newItem: function (e) {
       e.preventDefault();
-      var item = this.add(),
-          $item = $(this.template.replace(/ID/g, item.id));
+      let item = this.add(); //Create a new item
+      let itemHtml = this.template(item); //Create html for new item in table
+      let itemElement = document.createElement("tr"); //Create new element to house html
+      itemElement.innerHTML = itemHtml; //Add html to item element
 
-      $("#inventory").append($item);
+      //Add newly created item element to table
+      document
+        .getElementById("inventory")
+        .firstElementChild.appendChild(itemElement);
     },
-    findParent: function(e) {
-      return $(e.target).closest("tr");
+    findParent: function (e) {
+      return e.target.closest("tr");
     },
-    findID: function($item) {
-      return +$item.find("input[type=hidden]").val();
+    findID: function (item) {
+      return +item.querySelector("input[type=hidden]").value;
     },
-    deleteItem: function(e) {
+    deleteItem: function (e) {
+      //Only delete when clicking on a link with the delete class
+      if (!e.target.tagName === "A" || !e.target.classList.contains("delete"))
+        return;
+
       e.preventDefault();
-      var $item = this.findParent(e).remove();
+      let item = this.findParent(e); //Get parent tr
+      item.remove(); //Remove tr
 
-      this.remove(this.findID($item));
+      this.remove(this.findID(item)); //Get id of item and remove it from collection
     },
-    updateItem: function(e) {
-      var $item = this.findParent(e);
+    updateItem: function (e) {
+      if (!["INPUT", "TEXTAREA", "SELECT", "BUTTON"].includes(e.tagName))
+        return;
 
-      this.update($item);
+      let item = this.findParent(e);
+
+      this.update(item);
     },
-    bindEvents: function() {
-      $("#add_item").on("click", $.proxy(this.newItem, this));
-      $("#inventory").on("click", "a.delete", $.proxy(this.deleteItem, this));
-      $("#inventory").on("blur", ":input", $.proxy(this.updateItem, this));
+    bindEvents: function () {
+      //Adds item creation event listener
+      document
+        .getElementById("add_item")
+        .addEventListener("click", this.newItem.bind(this));
+
+      //Item removal event listener
+      document
+        .getElementById("inventory")
+        .addEventListener("click", this.deleteItem.bind(this));
+
+      //Item update event listener
+      document
+        .getElementById("inventory")
+        .addEventListener("blur", this.updateItem.bind(this));
     },
-    init: function() {
+    init: function () {
       this.setDate();
       this.cacheTemplate();
       this.bindEvents();
-    }
+    },
   };
 })();
 
-$($.proxy(inventory.init, inventory));
+document.addEventListener("DOMContentLoaded", inventory.init.bind(inventory));

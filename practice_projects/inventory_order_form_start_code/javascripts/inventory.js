c4f2cbfa1e35d96198299193f4de819c2e377ab4
Benjamin Stevens <B3nbinoh@gmail.com>
2025-03-27 13:21:09 -0600

    - Reset refactoring assignment

--- practice_projects/inventory_order_form_start_code/javascripts/inventory.js
+++ practice_projects/inventory_order_form_start_code/javascripts/inventory.js
@@ -1,38 +1,38 @@
 var inventory;
 
-(function () {
+(function() {
   inventory = {
     lastId: 0,
     collection: [],
-    setDate: function () {
+    setDate: function() {
       var date = new Date();
       $("#order_date").text(date.toUTCString());
     },
-    cacheTemplate: function () {
+    cacheTemplate: function() {
       var $iTmpl = $("#inventory_item").remove();
       this.template = $iTmpl.html();
     },
-    add: function () {
+    add: function() {
       this.lastId++;
       var item = {
         id: this.lastId,
         name: "",
         stock_number: "",
-        quantity: 1,
+        quantity: 1
       };
       this.collection.push(item);
 
       return item;
     },
-    remove: function (idx) {
-      this.collection = this.collection.filter(function (item) {
+    remove: function(idx) {
+      this.collection = this.collection.filter(function(item) {
         return item.id !== idx;
       });
     },
-    get: function (id) {
+    get: function(id) {
       var found_item;
 
-      this.collection.forEach(function (item) {
+      this.collection.forEach(function(item) {
         if (item.id === id) {
           found_item = item;
           return false;
@@ -41,48 +41,48 @@ var inventory;
 
       return found_item;
     },
-    update: function ($item) {
+    update: function($item) {
       var id = this.findID($item),
-        item = this.get(id);
+          item = this.get(id);
 
       item.name = $item.find("[name^=item_name]").val();
       item.stock_number = $item.find("[name^=item_stock_number]").val();
       item.quantity = $item.find("[name^=item_quantity]").val();
     },
-    newItem: function (e) {
+    newItem: function(e) {
       e.preventDefault();
       var item = this.add(),
-        $item = $(this.template.replace(/ID/g, item.id));
+          $item = $(this.template.replace(/ID/g, item.id));
 
       $("#inventory").append($item);
     },
-    findParent: function (e) {
+    findParent: function(e) {
       return $(e.target).closest("tr");
     },
-    findID: function ($item) {
+    findID: function($item) {
       return +$item.find("input[type=hidden]").val();
     },
-    deleteItem: function (e) {
+    deleteItem: function(e) {
       e.preventDefault();
       var $item = this.findParent(e).remove();
 
       this.remove(this.findID($item));
     },
-    updateItem: function (e) {
+    updateItem: function(e) {
       var $item = this.findParent(e);
 
       this.update($item);
     },
-    bindEvents: function () {
+    bindEvents: function() {
       $("#add_item").on("click", $.proxy(this.newItem, this));
       $("#inventory").on("click", "a.delete", $.proxy(this.deleteItem, this));
       $("#inventory").on("blur", ":input", $.proxy(this.updateItem, this));
     },
-    init: function () {
+    init: function() {
       this.setDate();
       this.cacheTemplate();
       this.bindEvents();
-    },
+    }
   };
 })();
 

47fb5f7506d0db23871c8176f93b2c799ea01ed8
Benjamin Stevens <B3nbinoh@gmail.com>
2025-03-31 13:15:19 -0600

    Photo gallery 2 - Added prev and next slideshow button functionality

--- practice_projects/photo_gallery2/public/javascripts/gallery.js
+++ practice_projects/photo_gallery2/public/javascripts/gallery.js
@@ -9,30 +9,16 @@ $(() => {
   );
 
   let photos;
-
-  //Render html for slides, header, and comments
-  $.ajax("/photos")
-    .done((response) => {
-      photos = response;
-
-      //Render all photos slides
-      let photosHtml = photosTemplate({ photos });
-      document.getElementById("slides").innerHTML = photosHtml;
-
-      //Render current (1st) photo header and comments
-      renderHeader(photos[0].id);
-      renderComments(photos[0].id);
-    })
-    .fail(() => {
-      console.error("Request unsuccessful: GET /photos");
-    });
+  let currPhotoId;
+  let $prevButton = $(".prev");
+  let $nextButton = $(".next");
 
   //Gets and renders header of specified photo
   function renderHeader(photoId) {
     //Find specified photo's json from photos array
     let photoJson = photos.filter((obj) => obj.id === photoId)[0];
 
-    //Create and render html of header
+    //Create and render header html
     let headerHtml = photoInfoTemplate(photoJson);
     document.getElementById("photo_header").innerHTML = headerHtml;
   }
@@ -42,8 +28,92 @@ $(() => {
     $.ajax(`/comments?photo_id=${photoId}`).done((response) => {
       let comments = response;
 
+      //Create and render comments html
       let commentsHtml = photoCommentsTemplate({ comments });
       document.querySelector("#comments > ul").innerHTML = commentsHtml;
     });
   }
+
+  //Returns jQuery element containing only the figure element with the passed in id
+  function getPhoto(photoId) {
+    return $("figure").filter((_, fig) => Number(fig.dataset.id) === photoId);
+  }
+
+  //Returns id of photo previous to the id passed in
+  function getPrevPhotoId(currId) {
+    for (let i = 0; i < photos.length; i++) {
+      if (photos[i].id === currId) {
+        return photos[i - 1] ? photos[i - 1].id : photos[photos.length - 1].id;
+      }
+    }
+  }
+
+  //Returns id of photo next to the id passed in
+  function getNextPhotoId(currId) {
+    for (let i = 0; i < photos.length; i++) {
+      if (photos[i].id === currId) {
+        return photos[i + 1] ? photos[i + 1].id : photos[0].id;
+      }
+    }
+  }
+
+  //Inital render html for slides, header, and comments
+  $.ajax("/photos")
+    .done((response) => {
+      photos = response;
+
+      //Render all photos slides
+      let photosHtml = photosTemplate({ photos });
+      document.getElementById("slides").innerHTML = photosHtml;
+
+      //Render current (1st) photo header and comments
+      currPhotoId = photos[0].id;
+      renderHeader(currPhotoId);
+      renderComments(currPhotoId);
+    })
+    .fail(() => {
+      console.error("Request unsuccessful: GET /photos");
+    });
+
+  $prevButton.on("click", (event) => {
+    event.preventDefault();
+
+    //Get the current photo
+    let $currPhoto = getPhoto(currPhotoId);
+
+    //Change currPhotoId to reflect desired changes to DOM
+    currPhotoId = getPrevPhotoId(currPhotoId);
+
+    //Get the previous photo
+    let $prevPhoto = getPhoto(currPhotoId);
+
+    //Perform photo swap
+    $currPhoto.fadeOut();
+    $prevPhoto.fadeIn();
+
+    //Perform photo info and comments swap
+    renderHeader(currPhotoId);
+    renderComments(currPhotoId);
+  });
+
+  $nextButton.on("click", (event) => {
+    event.preventDefault();
+
+    //Get the current photo
+    let $currPhoto = getPhoto(currPhotoId);
+
+    //Change currPhotoId to reflect desired changes to DOM
+    currPhotoId = getNextPhotoId(currPhotoId);
+
+    //Get the previous photo
+    let $nextPhoto = getPhoto(currPhotoId);
+
+    //Perform photo swap
+    $currPhoto.fadeOut();
+    $nextPhoto.fadeIn();
+
+    //Perform photo info and comments swap
+    renderHeader(currPhotoId);
+    renderComments(currPhotoId);
+  });
 });

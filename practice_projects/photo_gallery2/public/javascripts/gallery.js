ee98b24a3d621ed06261d7f0edad3d679d5043fe
Benjamin Stevens <B3nbinoh@gmail.com>
2025-03-31 11:09:48 -0600

    Photo gallery 2 - Added function for rendering photo comments - Refactored initial header rendering into function

--- practice_projects/photo_gallery2/public/javascripts/gallery.js
+++ practice_projects/photo_gallery2/public/javascripts/gallery.js
@@ -4,22 +4,46 @@ $(() => {
   let photoInfoTemplate = Handlebars.compile($("#photo_information").html());
   let photoCommentsTemplate = Handlebars.compile($("#photo_comments").html());
   let photoCommentPartial = Handlebars.registerPartial(
-    "photo_comment",
+    "comment",
     $("#photo_comment").html()
   );
 
-  //GET /photos and render html for slides and description (header)
+  let photos;
+
+  //Render html for slides, header, and comments
   $.ajax("/photos")
     .done((response) => {
-      let photos = response;
+      photos = response;
 
+      //Render all photos slides
       let photosHtml = photosTemplate({ photos });
       document.getElementById("slides").innerHTML = photosHtml;
 
-      let headerHtml = photoInfoTemplate(photos[0]);
-      document.getElementById("photo_header").innerHTML = headerHtml;
+      //Render current (1st) photo header and comments
+      renderHeader(photos[0].id);
+      renderComments(photos[0].id);
     })
     .fail(() => {
       console.error("Request unsuccessful: GET /photos");
     });
+
+  //Gets and renders header of specified photo
+  function renderHeader(photoId) {
+    //Find specified photo's json from photos array
+    let photoJson = photos.filter((obj) => obj.id === photoId)[0];
+
+    //Create and render html of header
+    let headerHtml = photoInfoTemplate(photoJson);
+    document.getElementById("photo_header").innerHTML = headerHtml;
+  }
+
+  //Gets and renders comment section of specified photo
+  function renderComments(photoId) {
+    $.ajax(`/comments?photo_id=${photoId}`).done((response) => {
+      let comments = response;
+
+      let commentsHtml = photoCommentsTemplate({ comments });
+      document.querySelector("#comments > ul").innerHTML = commentsHtml;
+    });
+  }
 });

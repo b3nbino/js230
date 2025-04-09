37142705568618f1ad42ecc3ca12364d26f234bd
Benjamin Stevens <B3nbinoh@gmail.com>
2025-04-08 18:01:11 -0600

    WYSIWYG - Added JS for buttons - Altered button id values - Added/changed CSS

--- exercises/misc_gui_apps/wysiwyg/script.js
+++ exercises/misc_gui_apps/wysiwyg/script.js
@@ -1,1 +1,47 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  let buttons = document.getElementById("buttons");
+
+  buttons.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    let button = event.target;
+
+    switch (button.id) {
+      case "bold":
+        document.execCommand("bold");
+        break;
+      case "italic":
+        document.execCommand("italic");
+        break;
+      case "underline":
+        document.execCommand("underline");
+        break;
+      case "strikeThrough":
+        document.execCommand("strikeThrough");
+        break;
+      case "createLink":
+        let url = window.prompt(
+          "Please enter the URL you would like to link to:"
+        );
+        document.execCommand("createLink", false, url ?? "#");
+        break;
+      case "insertUnorderedList":
+        document.execCommand("insertUnorderedList");
+        break;
+      case "insertOrderedList":
+        document.execCommand("insertOrderedList");
+        break;
+      default:
+        document.getElementById("justifyLeft").classList.remove("active");
+        document.getElementById("justifyCenter").classList.remove("active");
+        document.getElementById("justifyRight").classList.remove("active");
+        document.getElementById("justifyFull").classList.remove("active");
+
+        document.execCommand(button.id);
+
+        break;
+    }
+
+    event.target.classList.toggle("active");
+  });
+});

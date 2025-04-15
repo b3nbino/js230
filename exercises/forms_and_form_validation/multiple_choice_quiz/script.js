3a1bf0ea5a9fa8f0c2dd8e9f795f871753e27813
Benjamin Stevens <B3nbinoh@gmail.com>
2025-04-15 15:49:19 -0600

    Quiz - Worked it out somehow - Added submition and reset event listeners - Added some styles for answers - Changed answer HTML

--- exercises/forms_and_form_validation/multiple_choice_quiz/script.js
+++ exercises/forms_and_form_validation/multiple_choice_quiz/script.js
@@ -36,15 +36,67 @@ const answerKey = {
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  let questionTemplate = Handlebars.compile(
-    document.getElementById("question-template").getHTML()
-  );
-  Handlebars.registerPartial(
-    "choices",
-    document.getElementById("choices-partial").getHTML()
-  );
+  //Add quiz questions to DOM
+  {
+    let questionTemplate = Handlebars.compile(
+      document.getElementById("question-template").getHTML()
+    );
+    Handlebars.registerPartial(
+      "choices",
+      document.getElementById("choices-partial").getHTML()
+    );
 
-  let quiz = questionTemplate({ questions });
+    let quiz = questionTemplate({ questions });
 
-  document.getElementById("questions").innerHTML = quiz;
+    document.getElementById("questions").innerHTML = quiz;
+  }
+
+  let form = document.getElementById("quiz");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let submitButton = document.getElementById("submit");
+    submitButton.setAttribute("disabled", true);
+    let data = new FormData(form);
+    let values = data.entries();
+
+    values.forEach((question) => {
+      let id = question[0][1];
+      let answer = document.getElementById(`answer${id}`);
+
+      if (question[1] === "null") {
+        answer.textContent = `You didn't answer this question. The correct answer is ${answerKey[id]}`;
+        answer.classList.add("wrong");
+      } else if (question[1] === answerKey[id]) {
+        answer.textContent = `Correct!`;
+        answer.classList.add("correct");
+      } else {
+        answer.textContent = `Wrong answer. The correct answer is ${answerKey[id]}`;
+        answer.classList.add("wrong");
+      }
+
+      answer.style.display = "block";
+    });
+  });
+
+  form.addEventListener("reset", (event) => {
+    event.preventDefault();
+
+    {
+      let questionTemplate = Handlebars.compile(
+        document.getElementById("question-template").getHTML()
+      );
+      Handlebars.registerPartial(
+        "choices",
+        document.getElementById("choices-partial").getHTML()
+      );
+
+      let quiz = questionTemplate({ questions });
+
+      document.getElementById("questions").innerHTML = quiz;
+    }
+
+    document.getElementById("submit").removeAttribute("disabled");
+  });
 });

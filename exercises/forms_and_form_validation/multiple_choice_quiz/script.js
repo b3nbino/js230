1259a618815cb6f6976ea566fc3e8d9d04f16b8f
Benjamin Stevens <B3nbinoh@gmail.com>
2025-04-15 13:31:54 -0600

    Quiz - Removed temp HTML - Added handlebars dependency - Added handlebars templates to HTML

--- exercises/forms_and_form_validation/multiple_choice_quiz/script.js
+++ exercises/forms_and_form_validation/multiple_choice_quiz/script.js
@@ -1,1 +1,39 @@
+const questions = [
+  {
+    id: 1,
+    description:
+      "Who is the author of <cite>The Hitchhiker's Guide to the Galaxy</cite>?",
+    options: [
+      "Dan Simmons",
+      "Douglas Adams",
+      "Stephen Fry",
+      "Robert A. Heinlein",
+    ],
+  },
+  {
+    id: 2,
+    description:
+      "Which of the following numbers is the answer to Life, the \
+                  Universe and Everything?",
+    options: ["66", "13", "111", "42"],
+  },
+  {
+    id: 3,
+    description: "What is Pan Galactic Gargle Blaster?",
+    options: ["A drink", "A machine", "A creature", "None of the above"],
+  },
+  {
+    id: 4,
+    description: "Which star system does Ford Prefect belong to?",
+    options: ["Aldebaran", "Algol", "Betelgeuse", "Alpha Centauri"],
+  },
+];
+
+const answerKey = {
+  1: "Douglas Adams",
+  2: "42",
+  3: "A drink",
+  4: "Betelgeuse",
+};
+
 document.addEventListener("DOMContentLoaded", () => {});

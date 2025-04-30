0859e8eedb0839946b500e074aa252b3ee443b07
Benjamin Stevens <B3nbinoh@gmail.com>
2025-04-30 14:00:43 -0600

    Contact Manager - Made add contact form - Added functionality to "Add Contact" buttons - Added functionality to "Cancel" button

--- contact_manager_node/public/javascripts/script.js
+++ contact_manager_node/public/javascripts/script.js
@@ -1,1 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  let noSection = document.getElementById("no-contacts");
+  let addSection = document.getElementById("add-contacts");
+  let contactsMain = document.getElementById("contacts");
+  let nav = document.querySelector("nav");
+  let addButtons = document.querySelectorAll("button.add");
+  let cancel = document.getElementById("cancel");
+  let submit = document.getElementById("submit-contact");
+
+  //Show the add contact page
+  for (let i = 0; i < addButtons.length; i++) {
+    addButtons[i].addEventListener("click", (event) => {
+      noSection.classList.toggle("hide");
+      nav.classList.toggle("hide");
+      addSection.classList.toggle("hide");
+      contactsMain.classList.toggle("hide");
+    });
+  }
+
+  //Hide the add contact page
+  cancel.addEventListener("click", (event) => {
+    noSection.classList.toggle("hide");
+    nav.classList.toggle("hide");
+    addSection.classList.toggle("hide");
+    contactsMain.classList.toggle("hide");
+  });
+});

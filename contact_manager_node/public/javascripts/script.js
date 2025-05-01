192d938613601f705a76083165cce2c7645c9d54
Benjamin Stevens <B3nbinoh@gmail.com>
2025-05-01 14:24:25 -0600

    Contacts Manager - Added edit contact form HTML - Altered CSS for forms to be more general - Added "Edit" button behavior - Added edit contact form behavior

--- contact_manager_node/public/javascripts/script.js
+++ contact_manager_node/public/javascripts/script.js
@@ -1,14 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   let noSection = document.getElementById("no-contacts");
   let addSection = document.getElementById("add-contacts");
+  let editSection = document.getElementById("edit-contacts");
   let contactsMain = document.getElementById("contacts");
   let nav = document.querySelector("nav");
   let addButtons = document.querySelectorAll("button.add");
-  let cancel = document.getElementById("cancel");
+  let cancelSubmit = document.getElementById("cancel-submit");
+  let cancelEdit = document.getElementById("cancel-edit");
   let createForm = document.getElementById("create");
+  let editForm = document.getElementById("edit");
+  let contacts;
+  let currId;
 
   async function renderContacts() {
-    let contacts = await fetch("/api/contacts").then((res) => res.json());
+    contacts = await fetch("/api/contacts").then((res) => res.json());
 
     if (contacts.length > 0) {
       noSection.classList.add("hide");
@@ -18,6 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
       let contactsHTML = contactTemplate({ contacts });
 
       contactsMain.innerHTML = contactsHTML;
+      contactsMain.classList.remove("hide");
     } else {
       contactsMain.innerHTML = null;
       noSection.classList.remove("hide");
@@ -30,19 +36,27 @@ document.addEventListener("DOMContentLoaded", () => {
   for (let i = 0; i < addButtons.length; i++) {
     addButtons[i].addEventListener("click", (event) => {
       noSection.classList.add("hide");
-      nav.classList.toggle("hide");
+      nav.classList.add("hide");
       addSection.classList.toggle("hide");
-      contactsMain.classList.toggle("hide");
+      contactsMain.classList.add("hide");
     });
   }
 
   //Hide the add contact page
-  cancel.addEventListener("click", (event) => {
-    nav.classList.toggle("hide");
-    addSection.classList.toggle("hide");
-    contactsMain.classList.toggle("hide");
+  cancelSubmit.addEventListener("click", (event) => {
+    nav.classList.remove("hide");
+    addSection.classList.add("hide");
+    renderContacts();
   });
 
+  //Hide the edit contact page
+  cancelEdit.addEventListener("click", (event) => {
+    nav.classList.remove("hide");
+    editSection.classList.add("hide");
+    renderContacts();
+  });
+
+  //Handles create form submition event
   createForm.addEventListener("submit", async (event) => {
     event.preventDefault();
 
@@ -60,7 +74,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }
 
-    let response = await fetch(createForm.action, {
+    fetch(createForm.action, {
       method: createForm.method,
       headers: {
         "content-type": "application/json; charset=utf-8",
@@ -69,29 +83,76 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     renderContacts();
-    contactsMain.classList.toggle("hide");
-    addSection.classList.toggle("hide");
-    nav.classList.toggle("hide");
+    addSection.classList.add("hide");
+    nav.classList.remove("hide");
     createForm.reset();
   });
 
+  //Handles edit form submition event
+  editForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    //Convert form values into object
+    let data = editForm.elements;
+    let json = {};
+
+    for (let i = 0; i < data.length; i++) {
+      let element = data[i];
+      let key = element.name;
+      let value = element.value;
+
+      if (element.type !== "submit") {
+        json[key] = value;
+      }
+    }
+
+    fetch(editForm.action + "/" + currId, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(json),
+    });
+
+    renderContacts();
+    editSection.classList.add("hide");
+    nav.classList.remove("hide");
+    editForm.reset();
+  });
+
+  //Handles edit and delete button clicks on contact
   contactsMain.addEventListener("click", (event) => {
     if (event.target.tagName !== "BUTTON") {
       return;
     }
 
-    let id = event.target.dataset.id;
+    currId = event.target.dataset.id;
 
     if (event.target.classList.contains("edit")) {
       //Open edit page for contact
+      nav.classList.add("hide");
+      contactsMain.classList.add("hide");
+      editSection.classList.remove("hide");
+
+      let contact = contacts.filter((obj) => obj.id === Number(currId))[0];
+      let elements = editForm.elements;
+
+      for (let i = 0; i < elements.length; i++) {
+        let element = elements[i];
+
+        if (element.type === "submit") {
+          continue;
+        }
+
+        element.value = contact[element.name];
+      }
     } else {
       if (window.confirm("Are you sure you want to delete this contact?")) {
-        fetch(`/api/contacts/${id}`, {
+        fetch(`/api/contacts/${currId}`, {
           method: "DELETE",
         });
       }
+      renderContacts();
     }
-
-    renderContacts();
   });
 });
